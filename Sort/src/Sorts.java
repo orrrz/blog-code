1c9ef9fa8a01ac31c50526ed8acb90c27e2833d5
hwb <icejammm@gmail.com>
2019-04-29 23:47:05 +0800

    更新归并排序

--- Sort/src/Sorts.java
+++ Sort/src/Sorts.java
@@ -207,10 +207,49 @@ public class Sorts {
     public static void multi(int n) {
         System.out.println(factor(n));
     }
-
-    public static int factor(int n) {
-        if (n <= 1) return 1;   // 终止条件
+    private static int factor(int n) {
+        if (n <= 1) return 1;
         return n * factor(n - 1);
+    }
+
+
+    // -----------------------------------------Merge Sort----------------------------------------
+    private static void mergeSort(int[] arr) {
+        int[] temp = new int[arr.length];   // 在排序前，先建好一个长度等于原数组长度的临时数组，避免递归中频繁开辟空间
+        mergeSort(arr, 0, arr.length - 1, temp);
+    }
+    private static void mergeSort(int[]arr, int left, int right, int[] temp) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(arr, left, mid, temp);
+            mergeSort(arr, mid + 1, right, temp);   // 右边归并排序，使得右子序列有序
+            merge(arr, left, mid, right, temp);     // 将两个有序子数组合并操作
+        }
+    }
+
+    private static void merge(int[] arr, int left, int mid, int right, int[] temp) {
+        int i = left;   // 左序列指针
+        int j = mid + 1;// 右序列指针
+        int t = 0;      // 临时数组指针
+        while (i <= mid && j <= right) {
+            if (arr[i] <= arr[j]) {
+                temp[t++] = arr[i++];
+            }else {
+                temp[t++] = arr[j++];
+            }
+        }
+        while (i <= mid) {  //将左边剩余元素填充进temp中
+            temp[t++] = arr[i++];
+        }
+        while (j <= right) {    //将右序列剩余元素填充进temp中
+            temp[t++] = arr[j++];
+        }
+        t = 0;
+        // 将temp中的元素全部拷贝到原数组中
+        while (left <= right) {
+            arr[left++] = temp[t++];
+        }
+
     }
 
 
@@ -224,9 +263,10 @@ public class Sorts {
 //        insertSort(arr);
 //        insertSort2(arr);
 //        shellSort1(arr);
-        shellSort2(arr);
+//        shellSort2(arr);
 //        quickSort2(arr, 0, arr.length - 1);
-        multi(5);
+        mergeSort(arr);
+//        multi(5);
         printArr(arr);
     }
 
